eb8ce141e156d726b2479e190e63a14540ff5067
MoAzmy <mohammed.alaaeldin@outlook.com>
2025-10-26 10:49:17 +0300

    created scripts/backup and log.log , this scripts can be modified to specify what to backup, and where , using rsync

---
+++ scripts/backup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# this script will backup to remote server
+# modify variables as needed
+# using rsync command
+
+
+# Variables
+Source_Dir="/root/onlineTests/kimitCourse/kimit-labs/"
+Log_File="log.log"
+Remote_Host="hol2@192.168.37.127"
+Remote_Dir="/home/hol2/remoteBackups/"
+
+# Function 
+
+perform_backup() {
+	rsync -avz "$Source_Dir" "$Remote_Host":"$Remote_Dir" >> "$Log_File" 2>&1
+	if [ $? -eq 0 ];
+	then
+		echo " Backup success !! : $(date) " >>  "$Log_File"
+	else
+		ecbo " Backup FAILURE !!!! : $(date) " >> "$Log_File"
+	fi
+}
+
+# Run the backup
+
+perform_backup
